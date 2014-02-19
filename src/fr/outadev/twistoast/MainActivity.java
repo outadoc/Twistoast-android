f32401486c406fa981d5d80c7f34c305204f67f6
outadoc <outadoc@gmail.com>
2014-02-19 22:23:55 +0100

    Begin implementation of navigation drawer

--- src/fr/outadev/twistoast/MainActivity.java
+++ src/fr/outadev/twistoast/MainActivity.java
@@ -1,8 +1,15 @@
 package fr.outadev.twistoast;
 
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 
 public class MainActivity extends Activity {
 
@@ -15,7 +22,38 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent(this, TwistoastPebbleService.class);
 		startService(intent);
 
-		
+		drawerEntries = getResources().getStringArray(R.array.drawer_entries);
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		drawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, drawerEntries));
+
+		drawerToggle = new ActionBarDrawerToggle(this,
+				drawerLayout, R.drawable.ic_drawer, R.string.action_ok,
+				R.string.action_delete) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+			}
+		};
+
+		// Set the drawer toggle as the DrawerListener
+		drawerLayout.setDrawerListener(drawerToggle);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+	}
+
+	private void selectItem(int position) {
+
 	}
 
 	@Override
@@ -27,5 +65,36 @@ public class MainActivity extends Activity {
 	protected void onPause() {
 		super.onPause();
 	}
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+	private String[] drawerEntries;
+	private DrawerLayout drawerLayout;
+	private ListView drawerList;
+	ActionBarDrawerToggle drawerToggle;
 
 }
