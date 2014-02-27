d640695795aeded1c4b689fc4d59bc4225b8c077
outadoc <outadoc@gmail.com>
2014-02-27 11:20:03 +0100

    Rename TwistoastOpenHelper

--- src/fr/outadev/twistoast/TwistoastDatabaseOpenHelper.java
+++ src/fr/outadev/twistoast/TwistoastDatabaseOpenHelper.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class TwistoastOpenHelper extends SQLiteOpenHelper {
+public class TwistoastDatabaseOpenHelper extends SQLiteOpenHelper {
 
 	private static final int DATABASE_VERSION = 1;
 	private static final String DATABASE_NAME = "twistoast.db";
@@ -13,7 +13,7 @@ public class TwistoastOpenHelper extends SQLiteOpenHelper {
 	private static final String DIRECTIONS_TABLE_CREATE = "CREATE TABLE twi_direction(dir_id TEXT, line_id TEXT, dir_name TEXT, PRIMARY KEY(dir_id, line_id), FOREIGN KEY(line_id) REFERENCES twi_line(line_id));";
 	private static final String STOPS_TABLE_CREATE = "CREATE TABLE twi_stop(stop_id INTEGER, line_id TEXT, dir_id TEXT, stop_name TEXT, PRIMARY KEY(stop_id, line_id, dir_id), FOREIGN KEY(dir_id, line_id) REFERENCES twi_direction(dir_id, line_id));";
 
-	TwistoastOpenHelper(Context context) {
+	TwistoastDatabaseOpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
