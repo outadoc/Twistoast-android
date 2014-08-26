d70268e605f1fa287d68ef1b56776e98d8e79dc9
outadoc <outadoc@gmail.com>
2014-08-26 23:13:39 +0200

    Add cancel button to webviews

--- twistoast/src/main/java/fr/outadev/twistoast/ui/WebViewFragment.java
+++ twistoast/src/main/java/fr/outadev/twistoast/ui/WebViewFragment.java
@@ -22,6 +22,7 @@ import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -38,6 +39,8 @@ import fr.outadev.twistoast.R;
 public class WebViewFragment extends Fragment {
 
 	private WebView webView;
+	private MenuItem itemRefresh;
+	private MenuItem itemCancel;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -57,6 +60,9 @@ public class WebViewFragment extends Fragment {
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		inflater.inflate(R.menu.webview, menu);
+		itemRefresh = menu.findItem(R.id.action_refresh_page);
+		itemCancel = menu.findItem(R.id.action_cancel_refresh);
+
 		super.onCreateOptionsMenu(menu, inflater);
 	}
 
@@ -66,6 +72,9 @@ public class WebViewFragment extends Fragment {
 		switch(item.getItemId()) {
 			case R.id.action_refresh_page:
 				webView.reload();
+				return true;
+			case R.id.action_cancel_refresh:
+				webView.stopLoading();
 				return true;
 			case R.id.action_open_website:
 				Intent i = new Intent(Intent.ACTION_VIEW);
@@ -94,17 +103,36 @@ public class WebViewFragment extends Fragment {
 			setWebViewClient(new WebViewClient() {
 
 				@Override
+				public void onPageStarted(WebView view, String url, Bitmap favicon) {
+					super.onPageStarted(view, url, favicon);
+
+					//hide refresh button
+					if(itemRefresh != null && itemCancel != null) {
+						itemRefresh.setVisible(false);
+						itemCancel.setVisible(true);
+					}
+				}
+
+				@Override
 				public void onPageFinished(WebView view, String url) {
 					super.onPageFinished(view, url);
+
+					//hide cancel button
+					if(itemRefresh != null && itemCancel != null) {
+						itemRefresh.setVisible(true);
+						itemCancel.setVisible(false);
+					}
+
+					//load some javascript that will hide the stuff we don't want on the page
 					view.loadUrl("javascript: var a = document.getElementsByClassName(\"title-div\");");
 					view.loadUrl("javascript: var b = document.getElementsByClassName(\"contenu\");");
 					view.loadUrl("javascript:" + Uri.encode("for(var i = a.length-1; i >= 0; i--) { (b[0] != null) ? b[0]" +
-							".removeChild(a[i]) : " +
-							"document.body.removeChild(a[i]); }"));
+							".removeChild(a[i]) : document.body.removeChild(a[i]); }"));
 				}
 
 			});
 
+			//settings
 			getSettings().setBuiltInZoomControls(true);
 			getSettings().setDisplayZoomControls(false);
 			getSettings().setJavaScriptEnabled(true);
