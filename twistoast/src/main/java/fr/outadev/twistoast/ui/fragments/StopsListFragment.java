095c8e87174b3b4e7f385bec2799a7a295a09142
outadoc <outadoc@gmail.com>
2014-11-29 18:54:18 +0100

    Revert back to 60 seconds refreshes, wasn't effective

--- twistoast/src/main/java/fr/outadev/twistoast/ui/fragments/StopsListFragment.java
+++ twistoast/src/main/java/fr/outadev/twistoast/ui/fragments/StopsListFragment.java
@@ -60,8 +60,8 @@ import fr.outadev.twistoast.ui.activities.MainActivity;
 
 public class StopsListFragment extends Fragment implements IStopsListContainer {
 
-	//Refresh automatically every 50 seconds. Should be enough to let the screen turn off by itself.
-	private final long REFRESH_INTERVAL = 50000L;
+	//Refresh automatically every 60 seconds.
+	private final long REFRESH_INTERVAL = 60000L;
 
 	private final Handler periodicRefreshHandler = new Handler();
 	private Runnable periodicRefreshRunnable;
