a4a7f280d108228cf4daff62074c58fd1593d3f3
outadoc <outadoc@gmail.com>
2015-02-08 22:13:24 +0100

    Add new receivers

---
+++ twistoast/src/main/java/fr/outadev/twistoast/TwistoastBootReceiver.java
@@ -0,0 +1,53 @@
+/*
+ * Twistoast - BootReceiver
+ * Copyright (C) 2013-2015  Baptiste Candellier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.outadev.twistoast;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+public class TwistoastBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+			// Set the alarm here.
+		}
+	}
+
+	public static void enable(Context context) {
+		ComponentName receiver = new ComponentName(context, TwistoastBootReceiver.class);
+		PackageManager pm = context.getPackageManager();
+
+		pm.setComponentEnabledSetting(receiver,
+				PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+				PackageManager.DONT_KILL_APP);
+	}
+
+	public static void disable(Context context) {
+		ComponentName receiver = new ComponentName(context, TwistoastBootReceiver.class);
+		PackageManager pm = context.getPackageManager();
+
+		pm.setComponentEnabledSetting(receiver,
+				PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+				PackageManager.DONT_KILL_APP);
+	}
+}
