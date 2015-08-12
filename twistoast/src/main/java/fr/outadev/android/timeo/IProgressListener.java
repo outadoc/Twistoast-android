be17bab109dd4e1fd78092f9f5f1bbdf2aa7a8f6
outadoc <outadoc@gmail.com>
2015-08-12 13:59:39 +0200

    Update copyright

--- twistoast/src/main/java/fr/outadev/android/timeo/IProgressListener.java
+++ twistoast/src/main/java/fr/outadev/android/timeo/IProgressListener.java
@@ -1,13 +1,13 @@
 /*
  * Twistoast - IProgressListener
- * Copyright (C) 2013-2014  Baptiste Candellier
+ * Copyright (C) 2013-2015 Baptiste Candellier
  *
- * This program is free software: you can redistribute it and/or modify
+ * Twistoast is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Twistoast is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -30,6 +30,6 @@ public interface IProgressListener {
 	 * @param current current progress, can't be greater than total
 	 * @param total   maximum progress
 	 */
-	public void onProgress(int current, int total);
+	void onProgress(int current, int total);
 
 }
--- twistoast/src/main/java/fr/outadev/twistoast/MainActivity.java
+++ twistoast/src/main/java/fr/outadev/twistoast/MainActivity.java
@@ -1,13 +1,13 @@
 /*
  * Twistoast - MainActivity
- * Copyright (C) 2013-2014  Baptiste Candellier
+ * Copyright (C) 2013-2015 Baptiste Candellier
  *
- * This program is free software: you can redistribute it and/or modify
+ * Twistoast is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Twistoast is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
