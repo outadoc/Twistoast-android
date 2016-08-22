4fa8b8e7534d33eabcd9598ce3ffe92488d5a6ac
outadoc <outadoc@gmail.com>
2016-08-23 00:57:19 +0200

    Add test framework

---
+++ keolisprovider/src/test/java/fr/outadev/android/transport/timeo/TimeoRequestHandlerTest.kt
@@ -0,0 +1,43 @@
+package fr.outadev.android.transport.timeo
+
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+
+class TimeoRequestHandlerTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @Test
+    fun testGetLines() {
+        fail()
+    }
+
+    @Test
+    fun testGetStops() {
+        fail()
+    }
+
+    @Test
+    fun testGetSingleSchedule() {
+        fail()
+    }
+
+    @Test
+    fun testGetMultipleSchedules() {
+        fail()
+    }
+
+    @Test
+    fun testCheckForOutdatedStops() {
+        fail()
+    }
+
+    @Test
+    fun testGetGlobalTrafficAlert() {
+        fail()
+    }
+}
