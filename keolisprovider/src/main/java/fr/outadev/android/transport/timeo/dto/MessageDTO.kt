b40c99965e92d3b8e6b195018f4d9fd703996832
outadoc <outadoc@gmail.com>
2016-08-22 23:59:42 +0200

    Added missing attributes in MessageDTO

--- keolisprovider/src/main/java/fr/outadev/android/transport/timeo/dto/MessageDTO.kt
+++ keolisprovider/src/main/java/fr/outadev/android/transport/timeo/dto/MessageDTO.kt
@@ -1,11 +1,14 @@
 package fr.outadev.android.transport.timeo.dto
 
+import org.simpleframework.xml.Attribute
 import org.simpleframework.xml.Element
 
 /**
  * Created by outadoc on 21/08/16.
  */
 class MessageDTO {
+    @field:Attribute(required = false) var id: Int = -1
+    @field:Attribute(required = false) var type: String? = null
     @field:Element(required = false) var titre: String? = null
     @field:Element(required = false) var texte: String? = null
     @field:Element var bloquant: Boolean = false
