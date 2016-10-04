6929b4ce1861944ba1b54a59087c58a04b03dbf7
Baptiste Candellier <outadoc@gmail.com>
2016-10-04 20:49:52 +0200

    Fix traffic message expansion

--- twistoast/src/main/java/fr/outadev/twistoast/RecyclerAdapterRealtime.kt
+++ twistoast/src/main/java/fr/outadev/twistoast/RecyclerAdapterRealtime.kt
@@ -23,7 +23,6 @@ import android.app.AlertDialog
 import android.app.NotificationManager
 import android.content.Context
 import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.support.design.widget.Snackbar
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -31,17 +30,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AlphaAnimation
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import fr.outadev.android.transport.timeo.*
 import fr.outadev.twistoast.background.BackgroundTasksManager
 import fr.outadev.twistoast.uiutils.Colors
-import fr.outadev.twistoast.uiutils.collapse
-import fr.outadev.twistoast.uiutils.expand
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.onClick
 import org.jetbrains.anko.uiThread
 
 /**
@@ -49,7 +41,7 @@ import org.jetbrains.anko.uiThread
  *
  * @author outadoc
  */
-class RecyclerAdapterRealtime(val activity: Activity, private val stopsList: MutableList<TimeoStop>, private val mStopsListContainer: IStopsListContainer, private val mParentView: View) : RecyclerView.Adapter<RecyclerAdapterRealtime.ViewHolder>(), IRecyclerAdapterAccess {
+class RecyclerAdapterRealtime(val activity: Activity, private val stopsList: MutableList<TimeoStop>, private val mStopsListContainer: IStopsListContainer, private val mParentView: View) : RecyclerView.Adapter<StopScheduleViewHolder>(), IRecyclerAdapterAccess {
 
     private val database: Database
     private val config: ConfigurationManager
@@ -143,12 +135,12 @@ class RecyclerAdapterRealtime(val activity: Activity, private val stopsList: Mut
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, i: Int): RecyclerAdapterRealtime.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, i: Int): StopScheduleViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.view_schedule_row, parent, false)
-        return ViewHolder(v)
+        return StopScheduleViewHolder(v)
     }
 
-    override fun onBindViewHolder(view: RecyclerAdapterRealtime.ViewHolder, position: Int) {
+    override fun onBindViewHolder(view: StopScheduleViewHolder, position: Int) {
         // Get the stop we're inflating
         val currentStopId = stopsList[position]
 
@@ -238,7 +230,7 @@ class RecyclerAdapterRealtime(val activity: Activity, private val stopsList: Mut
         }
     }
 
-    private val longClickListener = object : ViewHolder.IOnLongClickListener {
+    private val longClickListener = object : StopScheduleViewHolder.IOnLongClickListener {
 
         override fun onLongClick(view: View, position: Int): Boolean {
             if (!mStopsListContainer.isRefreshing) {
@@ -328,96 +320,6 @@ class RecyclerAdapterRealtime(val activity: Activity, private val stopsList: Mut
             notificationManager.cancel(Integer.valueOf(stop.id)!!)
 
             Snackbar.make(mParentView, mParentView.context.getString(R.string.notifs_disable_toast, stop.name), Snackbar.LENGTH_LONG).show()
-        }
-
-    }
-
-    /**
-     * Container for an item in the list. Here, this corresponds to a bus stop, and all the info
-     * displayed for it (schedules, and metadata).
-     */
-    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-
-        val container: LinearLayout
-        val rowLineIdContainer: FrameLayout
-
-        val rowLineId: TextView
-        val rowStopName: TextView
-        val rowDirectionName: TextView
-        val viewScheduleContainer: LinearLayout
-        val imgStopWatched: ImageView
-        val lineDrawable: GradientDrawable
-
-        val lblStopTrafficTitle: TextView
-        val lblStopTrafficMessage: TextView
-
-        val lblScheduleTime = arrayOfNulls<TextView>(NB_SCHEDULES_DISPLAYED)
-        val lblScheduleDirection = arrayOfNulls<TextView>(NB_SCHEDULES_DISPLAYED)
-
-        val viewStopTrafficInfoContainer: View
-
-        var isExpanded: Boolean
-
-        init {
-
-            val inflater = LayoutInflater.from(v.context)
-            container = v as LinearLayout
-
-            isExpanded = false
-
-            // Get references to the views
-            rowLineIdContainer = v.findViewById(R.id.rowLineIdContainer) as FrameLayout
-
-            rowLineId = v.findViewById(R.id.rowLineId) as TextView
-            rowStopName = v.findViewById(R.id.rowStopName) as TextView
-            rowDirectionName = v.findViewById(R.id.rowDirectionName) as TextView
-
-            viewScheduleContainer = v.findViewById(R.id.viewScheduleContainer) as LinearLayout
-            imgStopWatched = v.findViewById(R.id.imgStopWatched) as ImageView
-            lineDrawable = rowLineIdContainer.background as GradientDrawable
-
-            lblStopTrafficTitle = v.findViewById(R.id.lblStopTrafficTitle) as TextView
-            lblStopTrafficMessage = v.findViewById(R.id.lblStopTrafficMessage) as TextView
-
-            viewStopTrafficInfoContainer = v.findViewById(R.id.viewStopTrafficInfoContainer)
-
-            // Stop traffic info is collapsed by default.
-            // When it's clicked, we display the message.
-            viewStopTrafficInfoContainer.onClick {
-                if (!isExpanded)
-                    lblStopTrafficMessage.expand()
-                else
-                    lblStopTrafficMessage.collapse()
-
-                lblStopTrafficMessage.requestLayout()
-                isExpanded = !isExpanded
-            }
-
-            for (i in 0..NB_SCHEDULES_DISPLAYED - 1) {
-                // Display the current schedule
-                val singleScheduleView = inflater.inflate(R.layout.view_single_schedule_label, null)
-
-                lblScheduleTime[i] = singleScheduleView.findViewById(R.id.lbl_schedule) as TextView
-                lblScheduleDirection[i] = singleScheduleView.findViewById(R.id.lbl_schedule_direction) as TextView
-
-                viewScheduleContainer.addView(singleScheduleView)
-            }
-        }
-
-        fun resetView() {
-            // Clear any previous data
-            for (i in 0..NB_SCHEDULES_DISPLAYED - 1) {
-                lblScheduleTime[i]?.text = ""
-                lblScheduleDirection[i]?.text = ""
-            }
-
-            viewStopTrafficInfoContainer.visibility = View.GONE
-            lblStopTrafficMessage.layoutParams.height = 0
-            isExpanded = false
-        }
-
-        interface IOnLongClickListener {
-            fun onLongClick(view: View, position: Int): Boolean
         }
 
     }
