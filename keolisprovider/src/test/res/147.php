e365e4968704897c4dc88c9eafa0a73c8552f66d
Baptiste Candellier <outadoc@gmail.com>
2016-10-22 12:02:00 +0200

    Add some random testing data, may be useful in the future

---
+++ keolisprovider/src/test/res/147.php
@@ -0,0 +1,231 @@
+<?xml version="1.0" encoding="utf-8" ?>
+
+<xmldata>
+  <erreur code="000"></erreur>
+
+  <heure>22:11</heure>
+  <date>2016-08-25</date>
+
+  <reseau>
+    <titre></titre>
+    <texte></texte>
+    <bloquant>false</bloquant>
+  </reseau>
+
+  <horaires nb="13">
+    <horaire id="1">
+      <description>
+        <code>5252</code>
+        <arret>Ecole de Louvigny</arret>
+        <ligne>18</ligne>
+        <ligne_nom>Ligne 18</ligne_nom>
+        <sens>A</sens>
+        <vers>RUE MICHELET</vers>
+        <couleur>#cc9933</couleur>
+      </description>
+
+      <passages nb="0">
+      </passages>
+
+      <messages nb="1">
+        <message id="1" type="la">
+          <titre>Déviation imprévue</titre>
+          <texte>En raison de travaux rue Bosnières, une déviation est mise en place sur les lignes 7, 14 et 18. Nombreux arrêts non desservis, consultez les infos trafic sur twisto.fr. Merci de votre compréhension.</texte>
+          <bloquant>false</bloquant>
+        </message>
+      </messages>
+    </horaire>
+    <horaire id="2">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="3">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="4">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="5">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="6">
+      <description>
+        <code>922</code>
+        <arret>CHARDONNERETS</arret>
+        <ligne>1</ligne>
+        <ligne_nom>Lianes 1</ligne_nom>
+        <sens>R</sens>
+        <vers>CAEN CHEMIN VERT BOURGOGNE</vers>
+        <couleur>#cc0099</couleur>
+      </description>
+
+      <passages nb="2">
+        <passage id="1">
+          <duree>22:51</duree>
+          <destination>CH. VERT BOURGOGNE</destination>
+        </passage>
+        <passage id="2">
+          <duree>23:27</duree>
+          <destination>CH. VERT BOURGOGNE</destination>
+        </passage>
+      </passages>
+
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="7">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="8">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="9">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="10">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="11">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="12">
+      <description>
+        <code>2941</code>
+        <arret>SATDE PRESTAVOINE</arret>
+        <ligne>5</ligne>
+        <ligne_nom>Ligne 5</ligne_nom>
+        <sens>R</sens>
+        <vers>MONDEVILLE CHARLOTTE CORDAY</vers>
+        <couleur>#ccccff</couleur>
+      </description>
+
+      <passages nb="0">
+      </passages>
+
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="13">
+      <description>
+        <code></code>
+        <arret></arret>
+        <ligne></ligne>
+        <ligne_nom></ligne_nom>
+        <sens></sens>
+        <vers></vers>
+        <couleur>#000000</couleur>
+      </description>
+
+      <passages nb="0"></passages>
+      <messages nb="0">
+      </messages>
+    </horaire>
+  </horaires>
+</xmldata>
---
+++ keolisprovider/src/test/res/147_08_26.php
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="utf-8" ?>
+
+<xmldata>
+  <erreur code="000"></erreur>
+
+  <heure>23:52</heure>
+  <date>2016-08-26</date>
+
+  <reseau>
+    <titre>Nouveaux horaires</titre>
+    <texte>Nouveaux horaires Twisto à partir du lundi 29 août. + infos sur twisto.fr</texte>
+    <bloquant>false</bloquant>
+  </reseau>
+
+  <horaires nb="6">
+    <horaire id="1">
+      <description>
+        <code>2523</code>
+        <arret>ACADEMIE</arret>
+        <ligne>TRAM</ligne>
+        <ligne_nom>TRAM</ligne_nom>
+        <sens>A</sens>
+        <vers>IFS - GRACE DE DIEU</vers>
+        <couleur>#ff3300</couleur>
+      </description>
+
+      <passages nb="2">
+        <passage id="1">
+          <duree>05:09</duree>
+          <destination>B  CAEN GRACE DE DIEU</destination>
+        </passage>
+        <passage id="2">
+          <duree>05:28</duree>
+          <destination>B  CAEN GRACE DE DIEU</destination>
+        </passage>
+      </passages>
+
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="2">
+      <description>
+        <code>131</code>
+        <arret>BERNIERES</arret>
+        <ligne>TRAM</ligne>
+        <ligne_nom>TRAM</ligne_nom>
+        <sens>A</sens>
+        <vers>IFS - GRACE DE DIEU</vers>
+        <couleur>#ff3300</couleur>
+      </description>
+
+      <passages nb="2">
+        <passage id="1">
+          <duree>23:53</duree>
+          <destination>A  IFS JEAN VILAR</destination>
+        </passage>
+        <passage id="2">
+          <duree>00:05</duree>
+          <destination>B  CAEN GRACE DE DIEU</destination>
+        </passage>
+      </passages>
+
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="3">
+      <description>
+        <code>8052</code>
+        <arret>RUE HAUTE</arret>
+        <ligne>NZCA</ligne>
+        <ligne_nom>NavCA</ligne_nom>
+        <sens>A</sens>
+        <vers>ZI DE CARPIQUET</vers>
+        <couleur>#cccccc</couleur>
+      </description>
+
+      <passages nb="0">
+      </passages>
+
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="4">
+      <description>
+        <code>543</code>
+        <arret>AEROPORT</arret>
+        <ligne>3</ligne>
+        <ligne_nom>Lianes 3</ligne_nom>
+        <sens>A</sens>
+        <vers>HEROUVILLE ST-CLAIR</vers>
+        <couleur>#339933</couleur>
+      </description>
+
+      <passages nb="2">
+        <passage id="1">
+          <duree>00:01</duree>
+          <destination>HEROUVILLE ST-CLAIR</destination>
+        </passage>
+        <passage id="2">
+          <duree>05:25</duree>
+          <destination>HEROUVILLE ST-CLAIR</destination>
+        </passage>
+      </passages>
+
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="5">
+      <description>
+        <code>2062</code>
+        <arret>AVIATION</arret>
+        <ligne>6</ligne>
+        <ligne_nom>Ligne 6</ligne_nom>
+        <sens>A</sens>
+        <vers>CAEN POINCARE</vers>
+        <couleur>#9999cc</couleur>
+      </description>
+
+      <passages nb="0">
+      </passages>
+
+      <messages nb="0">
+      </messages>
+    </horaire>
+    <horaire id="6">
+      <description>
+        <code>5421</code>
+        <arret>50 ACRES</arret>
+        <ligne>11</ligne>
+        <ligne_nom>Ligne 11</ligne_nom>
+        <sens>A</sens>
+        <vers>CUVERVILLE MAIRIE</vers>
+        <couleur>#666633</couleur>
+      </description>
+
+      <passages nb="0">
+      </passages>
+
+      <messages nb="0">
+      </messages>
+    </horaire>
+  </horaires>
+</xmldata>
