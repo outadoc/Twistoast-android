ca961df4a3b0265dd4e52671b357b2865f7e979b
Baptiste Candellier <outadoc@gmail.com>
2016-11-19 17:39:10 +0100

    Fix WebView header injection, fix opening page in external web browser

--- twistoast/src/main/kotlin/fr/outadev/twistoast/FragmentWebView.kt
+++ twistoast/src/main/kotlin/fr/outadev/twistoast/FragmentWebView.kt
@@ -28,6 +28,7 @@ import android.view.*
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
+
 /**
  * A fragment that contains a webview and its controls.
  * Will automatically inject some JS in the pages to make their title disappear, for cosmetic reasons.
@@ -51,21 +52,31 @@ class FragmentWebView : Fragment() {
             webView = TwistoastWebView(activity)
 
             if (arguments.containsKey("url")) {
+                // Load a classic URL
                 loadUrl(arguments.getString("url"))
             } else if (arguments.containsKey("twitter_username")) {
+                // Load a Twitter profile
                 loadTwitterTimeline(arguments.getString("twitter_username"))
             }
         }
+
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }*/
 
         return webView
     }
 
     private fun loadTwitterTimeline(username: String) {
+        // Load a twitter profile page by injecting the username into some HTML that will be loaded
+        // by the webview
         urlToOpen = "https://twitter.com/$username"
-        webView?.loadData("<html><body><a class=\"twitter-timeline\" href=\"https://twitter.com/$username\">" +
-                "Tweets by TwistoCaen</a> " +
-                "<script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script></body></html>",
-                "text/html", "utf-8")
+
+        val twitterHtmlWrapper = """<html><body><a class="twitter-timeline" href="https://twitter.com/$username">""" +
+                """Tweets by TwistoCaen</a> """ +
+                """<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></body></html>"""
+
+        webView?.loadData(twitterHtmlWrapper, "text/html", "utf-8")
     }
 
     private fun loadUrl(url: String) {
@@ -125,7 +136,7 @@ class FragmentWebView : Fragment() {
                 override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
                     super.onPageStarted(view, url, favicon)
 
-                    //hide refresh button
+                    // Hide refresh button while refreshing
                     itemRefresh?.isVisible = false
                     itemCancel?.isVisible = true
                 }
@@ -133,20 +144,31 @@ class FragmentWebView : Fragment() {
                 override fun onPageFinished(view: WebView, url: String) {
                     super.onPageFinished(view, url)
 
-                    //hide cancel button
+                    // Hide cancel button when done refreshing
                     itemRefresh?.isVisible = true
                     itemCancel?.isVisible = false
 
-                    //load some javascript that will hide the stuff we don't want on the page
-                    view.loadUrl("javascript: var a = document.getElementsByClassName(\"title-div\");")
-                    view.loadUrl("javascript: var b = document.getElementsByClassName(\"contenu\");")
+                    // Load some javascript that will hide the stuff we don't want on the page
+                    view.loadUrl("""javascript: var a = document.getElementsByClassName("title-div");""")
+                    view.loadUrl("""javascript: var b = document.getElementsByClassName("contenu");""")
                     view.loadUrl("javascript:" + Uri.encode("for(var i = a.length-1; i >= 0; i--) { " +
                             "(b[0] != null) ? b[0].removeChild(a[i]) : document.body.removeChild(a[i]); }"))
                 }
 
+                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                    if (Regex("^https?:").find(url) != null) {
+                        urlToOpen = url
+                    }
+
+                    // Intercept page load so that we can inject our own X-Requested-With header
+                    // because the Twisto website checks to see if it's coming from their own app
+                    // (and as you can see it's useless as hell)
+                    view.loadUrl(url, headers)
+                    return true
+                }
+
             })
 
-            //settings
             settings.builtInZoomControls = true
             settings.displayZoomControls = false
             settings.javaScriptEnabled = true
@@ -155,6 +177,8 @@ class FragmentWebView : Fragment() {
         override fun loadUrl(url: String) {
             super.loadUrl(url, headers)
 
+            // If this is a http or https URL, remember it
+            // so that we can open it in a browser later on if needed
             if (Regex("^https?:").find(url) != null) {
                 urlToOpen = url
             }
